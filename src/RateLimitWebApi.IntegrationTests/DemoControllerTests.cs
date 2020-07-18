78e13e699738f8cb14d1150e934b788791dc98c3
JessieXuCoverMore <66448829+JessieXuCoverMore@users.noreply.github.com>
2020-07-18 11:30:36 +1000

    Initial setup of projects skeleton

---
+++ src/RateLimitWebApi.IntegrationTests/DemoControllerTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RateLimitWebApi.IntegrationTests
+{
+    [TestClass]
+    public class DemoControllerTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ src/RateLimitWebApi/Controllers/DemoController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace RateLimitWebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DemoController : ControllerBase
+    {
+        private readonly ILogger<DemoController> _logger;
+
+        public DemoController(ILogger<DemoController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult<Task> Get()
+        {
+            return Ok("Hello AirTasker!");
+        }
+    }
+}
